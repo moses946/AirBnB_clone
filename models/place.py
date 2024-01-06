c96dba5643a7611970c3d313e4e52c49044e97f3
moses946 <mosesmwaiw@gmail.com>
2024-01-06 19:11:58 +0300

    Fix declaration of class attributes

--- models/place.py
+++ models/place.py
@@ -22,16 +22,17 @@ class Place(BaseModel):
         longitude(float)
         amenity_ids(list): List of Amenity.id
     """
+    city_id = ''
+    user_id = ''
+    name = ''
+    description = ''
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guests = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
-        self.city_id = ''
-        self.user_id = ''
-        self.name = ''
-        self.description = ''
-        self.number_rooms = 0
-        self.number_bathrooms = 0
-        self.max_guests = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
